8471c09638b83f5e044a4891301efb02d18a4c3f
afrika.williams@gmail.com <afrika.williams@gmail.com>
2017-11-14 13:18:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ printBadges(employee);
 
 function tailsNeverFails() {
     let tails = 0;
-    while (Math.random() >= 0.5); {
-      tails++;
-    }
+    while (Math.random() >= 0.5) {
+      tails++
+    };
     return `You got ${tails} tails in a row!`;
 }
